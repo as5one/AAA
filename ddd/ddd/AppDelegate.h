1dd9bbf4db69ebea9a4a60803c23c6cdd5d7529c
asone <18673748923@163.com>
2015-10-19 21:00:51 +0800

    初始化项目

---
+++ ddd/ddd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ddd
+//
+//  Created by xuejing on 15/10/19.
+//  Copyright (c) 2015年 asone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ddd/ddd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ddd
+//
+//  Created by xuejing on 15/10/19.
+//  Copyright (c) 2015年 asone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
